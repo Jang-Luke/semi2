73fc721654e669f68e29e8f1c2065805f0631711
mainlib990 <mainlib990@gmail.com>
2023-05-03 23:49:39 +0900

    feat(filter): 기초 필터 구현

---
+++ src/main/java/kr/co/khacademy/semi2/filter/BaseFilter.java
@@ -0,0 +1,23 @@
+package kr.co.khacademy.semi2.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class BaseFilter implements Filter {
+
+    protected FilterConfig filterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("Initialize filter: {}", getClass().getSimpleName());
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void destroy() {
+        log.info("Destroy filter: {}", getClass().getSimpleName());
+    }
+}
