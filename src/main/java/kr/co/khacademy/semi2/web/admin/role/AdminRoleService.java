e85c09e7f074b3b7d89b7949495e4c04db03a732
mainlib990 <mainlib990@gmail.com>
2023-05-06 19:53:29 +0900

    feat(role): Service add 기능 구현

---
+++ src/main/java/kr/co/khacademy/semi2/web/admin/role/AdminRoleService.java
@@ -0,0 +1,8 @@
+package kr.co.khacademy.semi2.web.admin.role;
+
+import kr.co.khacademy.semi2.web.admin.role.add.AdminRoleAddRequest;
+
+public interface AdminRoleService {
+
+    long add(AdminRoleAddRequest adminRoleAddRequest);
+}
---
+++ src/main/java/kr/co/khacademy/semi2/web/admin/role/AdminRoleServiceImpl.java
@@ -0,0 +1,37 @@
+package kr.co.khacademy.semi2.web.admin.role;
+
+import kr.co.khacademy.semi2.common.DataSourceUtils;
+import kr.co.khacademy.semi2.dao.RoleDao;
+import kr.co.khacademy.semi2.model.role.Role;
+import kr.co.khacademy.semi2.web.admin.role.add.AdminRoleAddRequest;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class AdminRoleServiceImpl implements AdminRoleService {
+
+    private static final RoleDao roleDao = RoleDao.getInstance();
+
+    private static class AdminRoleServiceImplLazyHolder {
+        static final AdminRoleServiceImpl instance = new AdminRoleServiceImpl();
+    }
+
+    public static AdminRoleServiceImpl getInstance() {
+        return AdminRoleServiceImplLazyHolder.instance;
+    }
+
+    @Override
+    public long add(AdminRoleAddRequest adminRoleAddRequest) {
+        Role role = adminRoleAddRequest.toRole();
+        try (Connection connection = DataSourceUtils.getConnection()) {
+            long id = roleDao.create(role, connection);
+            connection.commit();
+            return id;
+        } catch (SQLException e) {
+            throw new AdminRoleException(e);
+        }
+    }
+}
