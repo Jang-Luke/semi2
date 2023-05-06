67d72b0d47128324388c0ea483e04346ba403cd5
mainlib990 <mainlib990@gmail.com>
2023-05-06 20:03:05 +0900

    test(role): Service item 테스트

--- src/test/java/kr/co/khacademy/semi2/web/admin/role/AdminRoleServiceImplTest.java
+++ src/test/java/kr/co/khacademy/semi2/web/admin/role/AdminRoleServiceImplTest.java
@@ -1,7 +1,9 @@
 package kr.co.khacademy.semi2.web.admin.role;
 
 import kr.co.khacademy.semi2.common.DataSourceUtils;
+import kr.co.khacademy.semi2.model.Role;
 import kr.co.khacademy.semi2.web.admin.role.add.AdminRoleAddRequest;
+import kr.co.khacademy.semi2.web.admin.role.item.AdminRoleItemRequest;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -13,6 +15,7 @@ import java.sql.SQLException;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
@@ -61,6 +64,66 @@ class AdminRoleServiceImplTest {
                     .permissions(new String[]{"admin-page", "user"})
                     .build(),
                 2L
+            ),
+            arguments(
+                AdminRoleAddRequest.builder()
+                    .name("1".repeat(255))
+                    .permissions(new String[]{"admin-page"})
+                    .build(),
+                3L
+            )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("받기 성공")
+    @Order(2)
+    public void testGetting(AdminRoleItemRequest adminRoleItemRequest, Role expected) {
+        Role role = adminRoleService.getItem(adminRoleItemRequest);
+        assertThat(role).isEqualTo(expected);
+    }
+
+    public Stream<Arguments> testGetting() {
+        return Stream.of(
+            arguments(
+                AdminRoleItemRequest.builder()
+                    .id("1")
+                    .build(),
+                Role.builder()
+                    .id(1)
+                    .name("1234")
+                    .permissions("admin-page")
+                    .build()
+            ),
+            arguments(
+                AdminRoleItemRequest.builder()
+                    .id("2")
+                    .build(),
+                Role.builder()
+                    .id(2)
+                    .name("1234")
+                    .permissions("admin-page:user")
+                    .build()
+            )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("받기 실패")
+    @Order(3)
+    public void testGettingFailureWithInvalidId(AdminRoleItemRequest adminRoleItemRequest) {
+        assertThatThrownBy(() -> adminRoleService.getItem(adminRoleItemRequest))
+            .isInstanceOf(AdminRoleException.class);
+    }
+
+    public Stream<Arguments> testGettingFailureWithInvalidId() {
+        return Stream.of(
+            arguments(
+                AdminRoleItemRequest.builder()
+                    .id("100")
+                    .build()
             )
         );
     }
